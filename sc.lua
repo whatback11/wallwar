f3d2e0899a7646d176d75a2706883a2fb015433d
whatback11 <122617175+whatback11@users.noreply.github.com>
2024-02-01 09:07:50 +0900

    Update sc.lua

--- sc.lua
+++ sc.lua
@@ -6,10 +6,10 @@ local function SNO(title,text,duration,...)
     Duration = duration;
   })
 end
-while true do
+--[[ while true do
 	wait(0.1)
 	SNO("개발중...","개발중입니다. 다음날이나 잠시후 시도해 보세요!!",2.5 )
-end
+end]]
 game.Players.ChildAdded:Connect(function(player)
   if not pcall (function()
   SendNotification("플레이어 들어옴",""..player.Name.."  서버에 접속했습니다",2.5 )
