40ba5c7b57511607bdf7f01208312b0a0e895b9e
whatback11 <122617175+whatback11@users.noreply.github.com>
2024-02-01 09:06:24 +0900

    Update sc.lua

--- sc.lua
+++ sc.lua
@@ -25,3 +25,20 @@ game.Players.ChildRemoved:Connect(function(player)
     print ("Error")
   end
 end)
+local openmain = Instance.new("Frame")
+openmain.Name = "openmain"
+openmain.Parent = PrisonBreakerV16
+openmain.BackgroundColor3 = Color3.new(1, 1, 1)
+openmain.Position = UDim2.new(0.0104873544, 0, 0.549140036, 0)
+openmain.Size = UDim2.new(0, 100, 0, 26)
+
+open.Name = "open"
+open.Parent = openmain
+open.BackgroundColor3 = Color3.new(1, 0, 0.498039)
+open.Position = UDim2.new(0, 0, -0.0299201012, 0)
+open.Size = UDim2.new(0, 100, 0, 26)
+open.Font = Enum.Font.GothamBlack
+open.Text = "열기"
+open.TextColor3 = Color3.new(0, 0, 0)
+open.TextSize = 18
+open.TextWrapped = true
